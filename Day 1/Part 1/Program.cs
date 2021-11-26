ded7b9effc24f610c0164947beff301298b2027a
Andrew Stobart <astobart@microsoft.com>
2021-11-26 15:42:12 -0600

    Day 1 dev work

---
+++ Day 1/Part 1/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Advent of Code 2021
+ * 
+ * Programmer: Andrew Stobart
+ * Date: 
+ * 
+ * Day 1 Part 1
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
