40fa7b5f54395a3b46bc241265955e6562a178d8
Andrew Stobart <astobart@microsoft.com>
2021-12-01 13:53:20 -0600

    Day 1 Part 1 complete.

--- Day 1/Part 1/Program.cs
+++ Day 1/Part 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Advent of Code 2021
  * 
  * Programmer: Andrew Stobart
- * Date: 
+ * Date: December 1, 2021
  * 
  * Day 1 Part 1
  * 
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Part_1
 {
@@ -20,8 +21,27 @@ namespace Part_1
     {
         static void Main(string[] args)
         {
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2021\Advent-of-Code-2021\Day 1\Part 1\input.txt");
 
+            var linesList = new List<string>(lines);
 
+            int currentReading = 0;
+            int previousReading = 0;
+            int numberOfIncreases = 0;
+
+            for (int count=1; count<linesList.Count; count++)
+            {
+                currentReading = Convert.ToInt32(linesList[count]);
+                previousReading = Convert.ToInt32(linesList[count -1]);
+
+                if (currentReading > previousReading)
+                {
+                    numberOfIncreases++;
+                }
+            }
+
+            Console.WriteLine("There are " + numberOfIncreases + " increases.");
+            
             Console.WriteLine("");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
