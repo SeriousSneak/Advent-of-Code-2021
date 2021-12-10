8e6606010374ec03edcb512d28acc1376b3d02b0
Andrew Stobart <astobart@microsoft.com>
2021-12-10 11:32:28 -0600

    Day 4 Part 1 complete.

--- Day 4/Part 1/Program.cs
+++ Day 4/Part 1/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Advent of Code 2021
  * 
  * Programmer: Andrew Stobart
- * Date: 
+ * Date: December 10, 2021
  * 
  * Day 4 Part 1
  * 
@@ -13,7 +13,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-
+using System.Collections;
+using System.Data;
 
 namespace Part_1
 {
@@ -21,43 +22,183 @@ namespace Part_1
     {
         static void Main(string[] args)
         {
-            //CONCEPT CODE
-            int[,] a1 = new int[4, 4] {
-                        {0, 1, 2, 3} ,
-                        {4, 5, 6, 7} ,
-                        {8, 9, 10, 11} ,
-                        {12, 13, 14, 15}
-                        };
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2021\Advent-of-Code-2021\Day 4\Part 1\input.txt");
+
+            int count = 0;
+            ArrayList callNumbers = new ArrayList();
+            string[,] bingoCard = new string[5, 5];
+            List<string[,]> bingoCardsList = new List<string[,]>();
+            int row = 0;
+
+            //read the data into variables
+            foreach (var line in lines)
+            {
+                string[] stringSeparators = new string[] {" ", ","};
+                string[] subStrings = line.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                //our first line contains the numbers that we will be calling
+                if (count == 0)
+                {
+                    for (int x=0; x < subStrings.Length; x++)
+                    {
+                        callNumbers.Add(subStrings[x]);
+                    }
+                    count++;
+                }
+                else if (line == "")
+                {
+                    row = 0;
+                }
+                //if statement to check for space, then you know you need to go onto the next card
+                else if (line != "")
+                {
+                    //populate card array one line at a time
+                    for (int loop=0; loop < 5; loop++)
+                    {
+                        bingoCard[row, loop] = subStrings[loop];
+                    }
+                    row++;
+
+                    //fill the last row of the card to all zeros
+                    if (row == 5)
+                    {
+                        //this puts a copy of bingoCard into the list, as opposed to putting a reference in which would cause the value added to the list change if the original object changes
+                        bingoCardsList.Add((string[,])bingoCard.Clone());
+                    }
+                }
+            }
 
 
-            int[,] a2 = new int[4, 4] {
-                        {20, 21, 22, 23} ,
-                        {24, 25, 26, 27} ,
-                        {28, 29, 210, 211} ,
-                        {212, 213, 214, 215}
-                        };
+            //process the data
+            int winner = 0;
+            int puzzleAnswer = 0;
+
+            for (int callNumberLoop = 0; callNumberLoop < callNumbers.Count; callNumberLoop++)
+            {
+                string lastCalledNumber = callNumbers[callNumberLoop].ToString();
+                for (int bingoCardNumber = 0; bingoCardNumber < bingoCardsList.Count; bingoCardNumber++)
+                {
+                    for (int cardRow = 0; cardRow < 5; cardRow++)
+                    {
+                        for (int cardCol = 0; cardCol < 5; cardCol++)
+                        {
+                            if (bingoCardsList[bingoCardNumber][cardRow,cardCol] == callNumbers[callNumberLoop].ToString())
+                            {
+                                //put "C-" in front of a number once it has been called
+                                bingoCardsList[bingoCardNumber][cardRow, cardCol] = "C-" + bingoCardsList[bingoCardNumber][cardRow, cardCol];
+                            }
+                        }
+                    }
 
 
-            List<int[,]> bingoCards = new List<int[,]>();
+                    //check if our card is a winner
+                    winner = 0;
+                    //check rows for a winner
+                    for (int rowCheck = 0; rowCheck < 5; rowCheck++)
+                    {
+                        //Console.WriteLine("Last called number " + lastCalledNumber + ". Checking card number " + bingoCardNumber + " row rowCheck.");
+                        for (int colCheck = 0; colCheck < 5; colCheck++)
+                        {
+                            //get the first character in the cell. If it starts with 'C' then that number has been called
+                            if (bingoCardsList[bingoCardNumber][rowCheck,colCheck][0] == 'C')
+                            {
+                                winner++;
+                                if (winner == 5)
+                                {
+                                    //we have a winner. Calculate the puzzle answer
+                                    puzzleAnswer = calculateAnswer(lastCalledNumber, bingoCardsList[bingoCardNumber]);
 
-            bingoCards.Add(a1);
-            bingoCards.Add(a2);
+                                    //Console.WriteLine("Winner found on row.");
+                                    //break out of the loop here
+                                    colCheck = 5;
+                                    rowCheck = 5;
+                                    bingoCardNumber = bingoCardsList.Count;
+                                    callNumberLoop = callNumbers.Count;
+                                }
+                            }
+                            //move on as this row will not be a winner if any cell does not have a C in it
+                            else
+                            {
+                                colCheck = 5;
+                                winner = 0;
+                            }
+                        }
+                    }
 
 
-            int bob = 0;
+                    //check cols for a winner
+                    if (winner != 5)
+                        //check cols for a winner
+                        for (int colCheck = 0; colCheck < 5; colCheck++)
+                        {
+                            //Console.WriteLine("Last called number " + lastCalledNumber + ". Checking card number " + bingoCardNumber + " col Check.");
+                            for (int rowCheck = 0; rowCheck < 5; rowCheck++)
+                            {
+                                //get the first character in the cell. If it starts with 'C' then that number has been called
+                                if (bingoCardsList[bingoCardNumber][rowCheck, colCheck][0] == 'C')
+                                {
+                                    winner++;
+                                    if (winner == 5)
+                                    {
+                                        //we have a winner. Calculate the puzzle answer
+                                        puzzleAnswer = calculateAnswer(lastCalledNumber, bingoCardsList[bingoCardNumber]);
+                                        //Console.WriteLine("Winner found on col.");
 
-            bob = bingoCards[0][1,3];
+                                        //break out of the loop here
+                                        colCheck = 5;
+                                        rowCheck = 5;
+                                        bingoCardNumber = bingoCardsList.Count;
+                                        callNumberLoop = callNumbers.Count;
+                                        puzzleAnswer = calculateAnswer(lastCalledNumber,bingoCardsList[bingoCardNumber]);
+                                    }
+                                }
+                                //move on as this row will not be a winner if any cell does not have a C in it
+                                else
+                                {
+                                    rowCheck = 5;
+                                    winner = 0;
+                                }
+                            }
+                        }
+                }
+            }
 
-            Console.WriteLine(bob);
-            //END CONCEPT CODE
-            
 
+            if (winner == 5)
+            {
+                Console.WriteLine("We have a winner!");
+                Console.WriteLine("The puzzle answer is " + puzzleAnswer);
+            }
+            else
+            {
+                Console.WriteLine("No cards won.");
+            }
 
-
-
+            watch.Stop();
             Console.WriteLine("");
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+        }
+
+        public static int calculateAnswer(string lastCalledNumber,string[,] winningBoard)
+        {
+            int answer = 0;
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (winningBoard[row,col][0] != 'C')
+                    {
+                        answer += Convert.ToInt32(winningBoard[row, col]);
+                    }
+                }
+            }
+
+            return (answer * Convert.ToInt32(lastCalledNumber));
         }
     }
 }
