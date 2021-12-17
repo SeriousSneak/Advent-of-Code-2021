6fe58768e6983d9b33afd0b16f1af552bfbab436
Andrew Stobart <astobart@microsoft.com>
2021-12-17 10:19:21 -0600

    Initial Day 6 work

---
+++ Day 6/Part 1/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Advent of Code 2021
+ * 
+ * Programmer: Andrew Stobart
+ * Date: 
+ * 
+ * Day 6 Part 1
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
+
+
+
+
+
+
+            watch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
