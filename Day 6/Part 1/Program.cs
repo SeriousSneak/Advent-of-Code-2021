5ac46d7b0d3ba8f71b825b9baf7ed78d0218c330
Andrew Stobart <astobart@microsoft.com>
2022-01-03 10:58:36 -0600

    Day 6 part 1 complete

--- Day 6/Part 1/Program.cs
+++ Day 6/Part 1/Program.cs
@@ -1,11 +1,12 @@
 ﻿/* Advent of Code 2021
  * 
  * Programmer: Andrew Stobart
- * Date: 
+ * Date: January 3, 2022
  * 
  * Day 6 Part 1
  * 
  */
+
 
 using System;
 using System.Collections.Generic;
@@ -23,14 +24,44 @@ namespace Part_1
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
 
+            var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2021\Advent-of-Code-2021\Day 6\Part 1\input.txt");
+            var linesList = new List<string>(lines);
 
+            //the input file has a single line, so I have hard coded index 0 here
+            string[] values = linesList[0].Split(',');
+            List<int> lanternFishList = new List<int>();
 
+            for (int x=0; x<values.Length; x++)
+            {
+                lanternFishList.Add(Convert.ToInt32(values[x]));
+            }
 
+            for (int loop = 0; loop < 80; loop++)
+            {
+                int addBaby = 0;
+                for (int fishNumber=0; fishNumber < lanternFishList.Count; fishNumber++)
+                {
+                    if (lanternFishList[fishNumber] == 0)
+                    {
+                        lanternFishList[fishNumber] = 6;
+                        addBaby++;
+                    }
+                    else
+                    {
+                        lanternFishList[fishNumber]--;
+                    }
+                }
+                for (int newChildren = 0; newChildren < addBaby; newChildren++)
+                {
+                    lanternFishList.Add(8);
+                }
+            }
 
-
-
+            
             watch.Stop();
-            Console.WriteLine();
+
+            Console.WriteLine("There would be " + lanternFishList.Count + " lantern fish.");
+            Console.WriteLine("");
             Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
--- Day 6/Part 1/Properties/AssemblyInfo.cs
+++ Day 6/Part 1/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Part 1")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e8b19a6c-d8d5-4ab7-92e7-16563da2f173")]
+[assembly: Guid("c72389b4-42bb-459d-b867-b1cf063b7227")]
 
 // Version information for an assembly consists of the following four values:
 //
