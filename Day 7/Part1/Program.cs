49ca3074f928eb4bd03afe66ea5bcb6ccf5c4bad
Andrew Stobart <andrew@Maui.local>
2022-01-09 00:28:59 -0600

    Day 7 Part 1 work.

--- Day 7/Part1/Program.cs
+++ Day 7/Part1/Program.cs
@@ -32,11 +32,48 @@ namespace Part1
             //var lines = File.ReadLines(@"C:\Users\astobart\OneDrive\Work\Code\Advent of Code\2021\Advent-of-Code-2021\Day 6\Part 1\input.txt");
 
             //mac
-            //var lines = File.ReadLines(@"/Users/andrew/OneDrive/Work/Code/Advent of Code/2021/Advent-of-Code-2021/Day 6/Part 2/input.txt");
+            var lines = File.ReadLines(@"/Users/andrew/Temp/Advent-of-Code-2021/Day 7/Part1/input.txt");
 
+            var linesList = new List<string>(lines);
+
+            //the input file has a single line, so I have hard coded index 0 here
+            string[] values = linesList[0].Split(',');
+            List<int> crabValues = new List<int>();
+
+            for (int x=0; x<values.Length; x++)
+            {
+                crabValues.Add(Convert.ToInt32(values[x]));
+            }
+
+            int[] temp = crabValues.ToArray();
+
+            decimal medianValue = 0;
+            Array.Sort(temp);
+            int count = temp.Length;
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Empty collection");
+            }
+            else if (count % 2 == 0)
+            {
+                // count is even, average two middle elements
+                int a = temp[count / 2 - 1];
+                int b = temp[count / 2];
+                //return (a + b) / 2m;
+                medianValue = (a + b) / 2m;
+            }
+            else
+            {
+                // count is odd, return the middle element
+                //return temp[count / 2];
+                medianValue = temp[count / 2];
+            }
 
             watch.Stop();
 
+            //371 is not correct. This is actually the number they need to align to. I now need to figure out how much fuel is used and that will be the answer.
+
+            Console.WriteLine("The answer is " + medianValue + ".");
             Console.WriteLine("");
             Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
             Console.WriteLine("Press any key to continue.");
